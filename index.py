7bda1ffa0957dce9a27dc46e20172def8762e1d7
rushilpillamaricode <121631933+rushilpillamaricode@users.noreply.github.com>
2023-01-10 11:23:36 +0530

    Update index.py

--- index.py
+++ index.py
@@ -34,40 +34,4 @@ s.onkeypress(key="Right",fun=turn_r)
 s.onkeypress(key="Down",fun=move_b)
 s.onkeypress(key="space",fun=clear)
 s.onkeypress(key="a",fun=erase)
-s.exitonclick()from turtle import Turtle,Screen
-import keyboard
-
-t = Turtle()
-s = Screen()
-t.speed("fastest")
-
-def move_f():
-    t.forward(10)
-
-def move_b():
-    t.backward(10)
-
-def turn_l():
-    t.setheading(t.heading() + 10)
-
-def turn_r():
-    t.setheading(t.heading() - 10)
-
-def clear():
-    t.speed("slowest")
-    t.penup()
-    t.home()
-    t.pendown()
-    t.speed("fastest")
-    
-def erase():
-    t.clear()
-
-s.listen()
-s.onkeypress(key="Up",fun=move_f)
-s.onkeypress(key="Left",fun=turn_l)
-s.onkeypress(key="Right",fun=turn_r)
-s.onkeypress(key="Down",fun=move_b)
-s.onkeypress(key="space",fun=clear)
-s.onkeypress(key="a",fun=erase)
 s.exitonclick()
